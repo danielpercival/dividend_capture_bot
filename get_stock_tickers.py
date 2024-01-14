f2677e7d06927c07d33b20ec6866af540f3c5ad2
DanielP <daniel.percival.6@gmail.com>
2024-01-14 16:52:06 +0000

    Beat the cookie pop up on yahoo finance!

--- get_stock_tickers.py
+++ get_stock_tickers.py
@@ -1,5 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome('./chromedriver')
+from selenium.webdriver.common.by import By 
+from selenium.common.exceptions import NoSuchElementException
+import time 
+
+
+driver = webdriver.Chrome()
+# ...
+
 driver.get("https://finance.yahoo.com/screener/equity/new")
+time.sleep(2)
+driver.maximize_window()
+time.sleep(2)
+#cookie_button = driver.find_element("name", "q")
+element = driver.find_element(By.XPATH, "/html/body/div/div/div/div/form/div[2]/div[2]/button[1]")
+time.sleep(2)
+element.click()
+time.sleep(2)
+#...
+driver.quit()
 
