5123f203beaf604ca046024d6e83d3654c2120b2
DanielP <daniel.percival.6@gmail.com>
2024-01-23 20:10:03 +0000

    fixed one of two queries not working. Now can set a dividen minimum

--- query_dividend_table.py
+++ query_dividend_table.py
@@ -4,7 +4,7 @@ from get_all_tickers.get_tickers import Region
 import pandas as pd
 from datetime import datetime#
 import time 
-div_min = 0.1
+div_min = 0.01
 div_data = pd.read_csv('dividend_table.csv')
 unix_timestamp = datetime.now()
 other_time_stamp = time.time()
@@ -12,8 +12,10 @@ print(unix_timestamp)
 print(other_time_stamp)
 #print(div_data)
 div_data = div_data.dropna()
+div_data = div_data.astype({'Dividend_Yield': float})
 print(div_data)
-div_data = div_data.query('Dividend_Yield > @div_min' and 'UNIX_Div_Date > @other_time_stamp')
+div_data = div_data.query('Dividend_Yield > @div_min')
+div_data = div_data.query('UNIX_Div_Date > @other_time_stamp')
 print(div_data)
 div_data = div_data.sort_values(by=['UNIX_Div_Date'], ascending=True)
 div_data.to_csv('filtered_div_data.csv')
